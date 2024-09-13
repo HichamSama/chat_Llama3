8c462ec5d431ae8a9aa18bd7c88c4c2721decfe8
HESHAM <hicham.roh@gmail.com>
2024-09-13 18:03:12 +0100

    v1

---
+++ src/main/java/chat/pfe/mti/chatLlama3/ChatLlama3Application.java
@@ -0,0 +1,13 @@
+package chat.pfe.mti.chatLlama3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatLlama3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatLlama3Application.class, args);
+	}
+
+}
---
+++ src/test/java/chat/pfe/mti/chatLlama3/ChatLlama3ApplicationTests.java
@@ -0,0 +1,13 @@
+package chat.pfe.mti.chatLlama3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatLlama3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
